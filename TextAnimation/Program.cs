bbf4947531a96b76dc5c3fd0e4476ef66bc76668
dayofpay <vladigames03@gmail.com>
2022-09-18 18:03:58 +0300

    Import

---
+++ TextAnimation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnimation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> alphabet = new List<string>();
+            List<int> indexes = new List<int>();
+            string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower();
+            /*for(int i = 1; i<= alpha.Length; i++)
+            {
+                alphabet.Add(alpha[i-1].ToString());
+            }
+            */
+            Console.WriteLine("Your Text: ");
+            string myText = Console.ReadLine();
+            var type = Console.ReadLine();
+            if(type == "text")
+            {
+                for (int i = 1; i <= myText.Length; i++)
+                {
+                    Console.Write(alpha[alpha.IndexOf(myText[i - 1].ToString())]);
+                }
+            }
+            else if(type == "numeric")
+            {
+                for (int i = 1; i <= myText.Length; i++)
+                {
+                    Console.WriteLine(alpha.IndexOf(myText[i - 1].ToString()));
+                }
+            }
+        }
+    }
+}
